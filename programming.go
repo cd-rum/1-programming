47fc3a54909bbfc72c39c1548ca0e7f4c9533fef
ghost@mono <mono@endmail.xyz>
2020-06-29 10:16:53 +1000

    remade first function

--- programming.go
+++ programming.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world.")
+  fmt.Println(factorial(6))
 }
+
+func factorial(n int) int {
+  if n == 0 {
+    return 1
+  }
+  return n * factorial(n - 1)
+}
+
