89b2640175ad146f4d0b20ea602a7796c08376f9
ghost@mono <mono@endmail.xyz>
2020-06-29 10:25:52 +1000

    second factorial

--- programming.go
+++ programming.go
@@ -3,13 +3,24 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println(factorial(6))
+  fmt.Println(factorialOne(6))
+  fmt.Println(factorialTwo(6))
 }
 
-func factorial(n int) int {
+func factorialOne(n int) int {
   if n == 0 {
     return 1
   }
-  return n * factorial(n - 1)
+  return n * factorialOne(n - 1)
 }
 
+func factorialTwo(n int) int {
+  return factIter(1, 1, n)
+}
+
+func factIter(product int, counter int, n int) int {
+  if counter > n {
+    return product
+  }
+  return factIter(product * counter, counter + 1, n)
+}
